375c8258c15eff25b74bbb3edf46774167e3669e
maricaveress <maria.veress@srud.uni.corvinus.hu>
2024-04-11 16:50:39 +0200

    végeztem

---
+++ mintazh/FormCountryEdit.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace mintazh
+{
+    partial class FormCountryEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            bindingSource1 = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Country name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(35, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Population:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(35, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Area in km2:";
+            // 
+            // textBox1
+            // 
+            textBox1.DataBindings.Add(new Binding("Text", bindingSource1, "Name", true));
+            textBox1.Location = new Point(35, 73);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(118, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.DataBindings.Add(new Binding("Text", bindingSource1, "Population", true));
+            textBox2.Location = new Point(35, 123);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(118, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.DataBindings.Add(new Binding("Text", bindingSource1, "AreaInSquareKm", true));
+            textBox3.Location = new Point(35, 167);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(118, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // bindingSource1
+            // 
+            bindingSource1.DataSource = typeof(CountryData);
+            // 
+            // FormCountryEdit
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormCountryEdit";
+            Text = "FormCountryEdit";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private BindingSource bindingSource1;
+    }
+}
